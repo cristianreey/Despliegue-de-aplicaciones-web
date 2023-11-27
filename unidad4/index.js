edf3e58bdbfea655eddee4fe4908a390ec81d131
cristianreey <cris11salle@gmail.com>
2023-11-27 12:11:30 +0100

    conectado a base de datos

--- unidad4/index.js
+++ unidad4/index.js
@@ -8,6 +8,31 @@
 // Importamos las bibliotecas necesarias.
 // Concretamente el framework express.
 const express = require("express");
+const { MongoClient } = require("mongodb");
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "Concesionario";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("documents");
+
+  // the following code examples can be pasted here...
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
 
 // Inicializamos la aplicación
 const app = express();
